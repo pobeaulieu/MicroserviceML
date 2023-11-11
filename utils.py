55135af784d40a696399b5bfb0b08ac80884fcca
Pier-Olivier <pier-olivier.beaulieu.1@ens.etsmtl.ca>
2023-11-11 18:11:01 -0500

    Generate result csv for microservices

--- utils.py
+++ utils.py
@@ -113,7 +113,7 @@ def save_communities_to_csv(communities, version, system, algorithm):
                     f.write(f'{class_name},{label_type} Service {i + 1}\n')
 
 
-def save_microservices_to_file(fuzzy_clusters, communities, filename):
+def save_microservices_to_txt(fuzzy_clusters, communities, filename):
     """Saves and prints the clustered services and their corresponding classes to a file in a readable format.
 
     Parameters:
@@ -153,3 +153,34 @@ def save_microservices_to_file(fuzzy_clusters, communities, filename):
                     # Write and print the class entry
                     file.write(class_entry)
             file.write("\n")
+
+def save_microservices_to_csv(fuzzy_clusters, communities, csv_filename):
+    """Saves and prints the clustered services and their corresponding classes to a file in a readable format.
+
+    Parameters:
+    fuzzy_clusters (dict): Dictionary with services as keys and lists of tuples (cluster id, membership value) as values.
+    communities (pd.DataFrame): DataFrame with class names and their corresponding services.
+    filename (str): The name of the file to which the clusters and services will be saved.
+    csv_filename (str): The name of the CSV file to which class names will be saved.
+    """
+    # Determine the unique clusters
+    unique_clusters = set()
+    for memberships in fuzzy_clusters.values():
+        for cluster_id, _ in memberships:
+            unique_clusters.add(cluster_id)
+    unique_clusters = sorted(unique_clusters)
+
+    # Open the file to write the microservice clusters
+    with open(csv_filename, "w") as csv_file:
+        for cluster_num in unique_clusters:
+            # Find services that belong to the current cluster
+            cluster_services = [service for service, memberships in fuzzy_clusters.items() if any(cluster_id == cluster_num for cluster_id, _ in memberships)]
+            for service in cluster_services:
+                # Get related classes for the service from the communities dataframe
+                related_classes = communities[communities['service'] == service]['class_name'].tolist()
+                 # Write the class names to the CSV file, separated by commas
+                csv_file.write(",".join(related_classes))
+
+
+    
+            csv_file.write("\n")
