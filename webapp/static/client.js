747ac8670ad02765bda4e8b086f0a891bc4cdfc1
Pier-Olivier <pier-olivier.beaulieu.1@ens.etsmtl.ca>
2023-11-19 16:51:20 -0500

    Must provide url and call graph

--- webapp/static/client.js
+++ webapp/static/client.js
@@ -1,25 +1,21 @@
 // Wait for the DOM content to be fully loaded
-document.addEventListener('DOMContentLoaded', function () {
-    // Find the button element by its ID
-    var submitButton = document.getElementById('genms');
 
-    // Add a click event listener to the button
-    submitButton.addEventListener('click', function (event) {
-        // Call the submitForm function when the button is clicked
-        submitForm(event);
-    });
-});
 
 function submitForm(event) {
     event.preventDefault();
+
+    var form = document.getElementById('main_form')
+    var mainForm = new FormData(document.getElementById('main_form'));
+
+    if (!form.checkValidity()) {
+        alert('Please provide Github url and call graph!');
+        return
+    } 
 
     // Show modal
     document.getElementById('modal').style.display = 'flex';
 
       
-    // Get form data for the main form
-    var mainForm = new FormData(document.getElementById('main_form'));
-
     // Get form data for the optional parameters form
     var optionalParamsForm = new FormData(document.getElementById('optionnal_params'));
 
@@ -27,6 +23,7 @@ function submitForm(event) {
     for (var pair of optionalParamsForm.entries()) {
         mainForm.append(pair[0], pair[1]);
     }
+
 
     // Convert the FormData to a query string
     var queryString = new URLSearchParams(mainForm).toString();
