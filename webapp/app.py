a46e8555cb4a9553ca766f773421c87d9ac6178e
Pier-Olivier <pier-olivier.beaulieu.1@ens.etsmtl.ca>
2023-11-26 15:26:01 -0500

    Test

--- webapp/app.py
+++ webapp/app.py
@@ -1,7 +1,8 @@
+import asyncio
 from flask import Flask, render_template, request
 from flask_socketio import SocketIO
-import json
 import os, sys
+
 app = Flask(__name__)
 socketio = SocketIO(app)
 parent_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
@@ -10,7 +11,8 @@ parent_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
 sys.path.append(parent_dir)
 
 # Set the current working directory to the 'pipeline' directory
-from microminer.mock import MockImplementation
+from microminer.pipeline import MicroMinerPipeline
+from microminer.config.enums import Phase1EmbeddingModel, Phase1ClassifierModel, Phase2EmbeddingModel, Phase2ClusteringModel, Phase3ClusteringModel
 
 # Global variables to store results
 global_results = {
@@ -29,51 +31,57 @@ global_results = {
 @app.route('/')
 def index():
     return render_template('index.html')
-
+ 
 @socketio.on('pipeline')
 def pipeline(data):
     print("Received pipeline event:", data)
     # Handle the pipeline logic here
+
     repo_url = data['repo_url']
-    graph_path = data['call_graph_file']
+    graph_file = data['call_graph_file']
     phase1_model_llm = data['phase1_model_llm']
     phase1_model_ml = data['phase1_model_ml']
     phase2_model = data['phase2_model']
     num_microservices = data['num_microservices']
     phase3_model = data['phase3_model']
 
+    # socketio.emit('pipeline_progress', {'message': 'Cloning Repository'})
 
-    print(num_microservices)
-
-    pipeline = MockImplementation(
+    # Save the file to disk
+    with open('graph.csv', 'wb') as file:
+        file.write(graph_file)
+        
+    pipeline = MicroMinerPipeline(
         github_url=repo_url, 
-        embeddings_model_name_phase_1=phase1_model_llm, 
-        classification_model_name_phase_1= phase1_model_ml, 
-        clustering_model_name_phase_2= phase2_model, 
-        #call_graph= graph_path, 
-        clustering_model_name_phase_3= phase3_model, 
-        num_clusters= num_microservices, 
-        max_d= None, #TODO
-        embeddings_model_name_phase_2= None, #TODO
+        path_to_call_graph= "graph.csv", 
+        embeddings_model_name_phase_1=Phase1EmbeddingModel.codebert.name, 
+        classification_model_name_phase_1=Phase1ClassifierModel.svm.name,
+        embeddings_model_name_phase_2=Phase2EmbeddingModel.word2vec.name,
+        clustering_model_name_phase_2=Phase2ClusteringModel.GirvanNewman.name,
+        clustering_model_name_phase_3=Phase3ClusteringModel.custom_cmeans.name,
     )
 
-    # Emit messages back to the client
-    socketio.emit('pipeline_progress', {'message': 'Cloning Repository'})
-    socketio.sleep(1)
-    socketio.emit('pipeline_progress', {'message': 'Phase 1 in Progress'})
-    result1 = pipeline.execute_phase_1()
-    socketio.emit('pipeline_progress', {'message': 'Phase 2 in Progress'})
-    socketio.sleep(1)
-    result2 = pipeline.execute_phase_2()
-    socketio.sleep(1)
-    socketio.emit('pipeline_progress', {'message': 'Phase 3 in Progress'})
-    result3 = pipeline.execute_phase_3()
+    #socketio.emit('pipeline_progress', {'message': 'Cloning Repository'})
+    pipeline.clone_and_prepare_src_code()
+    #socketio.sleep(30)
+    #socketio.emit('pipeline_progress', {'message': 'Phase 1 in Progress'})
+    result1 =pipeline.execute_phase_1()
+    #socketio.sleep(30)
 
-    print(result1)
+    #socketio.emit('pipeline_progress', {'message': 'Phase 2 in Progress'})
+    result2 = pipeline.execute_phase_2()
+    #socketio.sleep(30)
+
+    #socketio.emit('pipeline_progress', {'message': 'Phase 3 in Progress'})
+    result3 = pipeline.execute_phase_3()
+    #socketio.sleep(30)
+
+
+    pipeline.clean_up()
+    
     # Update global results
     global_results.update({
         'repo_url': repo_url,
-        'graph_path': graph_path,
         'phase1_model_llm': phase1_model_llm,
         'phase1_model_ml': phase1_model_ml,
         'phase2_model': phase2_model,
@@ -84,7 +92,6 @@ def pipeline(data):
         'result3': result3
     })
 
-
     # Emit 'pipeline_complete' event with information
     socketio.emit('pipeline_complete')
 
@@ -92,7 +99,6 @@ def pipeline(data):
 def results():
     return render_template('results.html', **global_results)
 
-
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5001))
-    socketio.run(app, debug=True, port=port, host='0.0.0.0')
+    socketio.run(app, debug=True, port=port, host='127.0.0.1')
