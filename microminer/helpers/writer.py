7242b30abc79129ef675187bbae936dc57cb6099
Bianca Popa <56972854+BiancaP-hub@users.noreply.github.com>
2023-11-27 21:10:52 -0500

    generate metrics

--- microminer/helpers/writer.py
+++ microminer/helpers/writer.py
@@ -1,1 +1,32 @@
-import csv
+import pandas as pd
+import os
+from openpyxl import load_workbook
+
+def write_metrics_to_excel(data, file_path, sheet_name='Metrics'):
+    # Convert the data into a DataFrame
+    df = pd.DataFrame([data])
+
+    # Check if file exists
+    if os.path.exists(file_path):
+        # Load the workbook and the sheet
+        book = load_workbook(file_path)
+        writer = pd.ExcelWriter(file_path, engine='openpyxl')
+        writer.book = book
+
+        # Check if the sheet exists
+        if sheet_name in book.sheetnames:
+            # Load the existing sheet
+            writer.sheets = {ws.title: ws for ws in book.worksheets}
+            startrow = writer.sheets[sheet_name].max_row
+            
+            # Append data without overwriting the existing content
+            df.to_excel(writer, sheet_name=sheet_name, startrow=startrow, index=False, header=False)
+        else:
+            # Add new sheet with data
+            df.to_excel(writer, sheet_name=sheet_name, index=False)
+        
+        # Save the file
+        writer.save()
+    else:
+        # Write new file if it doesn't exist
+        df.to_excel(file_path, sheet_name=sheet_name, index=False)
---
+++ microminer/metrics/clustering_metrics.py
@@ -0,0 +1,101 @@
+def flatten_clusters(services, is_microservice=False):
+    """ Flatten services or microservices data into a list of sets. """
+    flattened = []
+    if is_microservice:
+        for microservice in services['microservices']:
+            cluster_set = set()
+            for service_type, service_groups in microservice['microservice'].items():
+                for service_group in service_groups:
+                    cluster_set.update([service['className'] for service in service_group['service']])
+            flattened.append(cluster_set)
+    else:
+        for service_groups in services.values():
+            for service_group in service_groups:
+                cluster_set = set([service['className'] for service in service_group['service']])
+                flattened.append(cluster_set)
+    return flattened
+
+def calculate_overlap(set1, set2):
+    """ Calculate the overlap ratio between two sets, as a number between 0 and 1. """
+    intersection = len(set1.intersection(set2))
+    smaller_set_size = min(len(set1), len(set2))
+    return (intersection / smaller_set_size) if smaller_set_size > 0 else 0
+
+def calculate_clustering_metrics(results, ground_truths, threshold=0.8, is_microservice=False):
+    """ Calculate metrics for clustering, with an option for type-specific metrics in Phase 2. """
+    metrics = {}
+    total_TP = total_FP = total_FN = 0
+
+    if is_microservice:
+        # For Phase 3 - Microservices
+        result_clusters = flatten_clusters(results, is_microservice)
+        ground_truth_clusters = [set(microservice) for microservice in ground_truths]
+        
+        for result_cluster in result_clusters:
+            overlaps = [calculate_overlap(result_cluster, gt_cluster) for gt_cluster in ground_truth_clusters]
+            best_overlap = max(overlaps) if overlaps else 0
+            if best_overlap >= threshold:
+                total_TP += 1
+            else:
+                total_FP += 1
+
+        for gt_cluster in ground_truth_clusters:
+            overlaps = [calculate_overlap(gt_cluster, result_cluster) for result_cluster in result_clusters]
+            best_overlap = max(overlaps) if overlaps else 0
+            if best_overlap < threshold:
+                total_FN += 1
+
+    else:
+        # For Phase 2 - Service Types
+        for service_type in results.keys():
+            result_clusters = flatten_clusters({service_type: results[service_type]})
+            ground_truth_clusters = flatten_clusters({service_type: ground_truths[service_type]})
+
+            TP = FP = FN = 0
+
+            for result_cluster in result_clusters:
+                overlaps = [calculate_overlap(result_cluster, gt_cluster) for gt_cluster in ground_truth_clusters]
+                best_overlap = max(overlaps) if overlaps else 0
+                if best_overlap >= threshold:
+                    TP += 1
+                else:
+                    FP += 1
+
+            for gt_cluster in ground_truth_clusters:
+                overlaps = [calculate_overlap(gt_cluster, result_cluster) for result_cluster in result_clusters]
+                best_overlap = max(overlaps) if overlaps else 0
+                if best_overlap < threshold:
+                    FN += 1
+
+            precision = TP / (TP + FP) if (TP + FP) > 0 else 0
+            recall = TP / (TP + FN) if (TP + FN) > 0 else 0
+            f1_score = 2 * (precision * recall) / (precision + recall) if (precision + recall) > 0 else 0
+
+            metrics[service_type] = {
+                'true_positives': TP,
+                'false_positives': FP,
+                'false_negatives': FN,
+                'precision': precision,
+                'recall': recall,
+                'f1_score': f1_score
+            }
+
+            total_TP += TP
+            total_FP += FP
+            total_FN += FN
+
+    # Calculate average precision, recall, and F1-score
+    avg_precision = total_TP / (total_TP + total_FP) if (total_TP + total_FP) > 0 else 0
+    avg_recall = total_TP / (total_TP + total_FN) if (total_TP + total_FN) > 0 else 0
+    avg_f1_score = 2 * (avg_precision * avg_recall) / (avg_precision + avg_recall) if (avg_precision + avg_recall) > 0 else 0
+
+    metrics['average'] = {
+        'total_true_positives': total_TP,
+        'total_false_positives': total_FP,
+        'total_false_negatives': total_FN,
+        'precision': avg_precision,
+        'recall': avg_recall,
+        'f1_score': avg_f1_score
+    }
+
+    return metrics
